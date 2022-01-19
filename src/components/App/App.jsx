2d4c75077ca4754775b59a9fc90dadd448f12fd2
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-01-19 22:57:27 +0530

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import './App.css';
+
+class App extends React.Component {
+    
+    render () {
+        return (
+            <Header />
+        )
+    }
+}
+
+export default App;
