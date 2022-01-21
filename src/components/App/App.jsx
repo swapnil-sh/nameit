5e368518f266725586434ab9982944d1094c47e0
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-01-22 01:36:49 +0530

    Added Animations

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import Header from "../Header/Header";
+import SearchBox from "../SearchBox/SearchBox";
 import './App.css';
 
 class App extends React.Component {
     
     state = {
         headerText: 'Name It!',
+        headerExpanded: true,
     };
+
+    handleInputChange = (inputText) => {
+        this.setState({ headerExpanded: inputText.length > 0 ? false : true });
+    }
 
     render () {
         return (
-            <Header headTitle={this.state.headerText}/>
+            <div>
+                <Header 
+                    headerExpanded={this.state.headerExpanded} 
+                    headTitle={this.state.headerText}
+                />
+                <SearchBox onInputChange={this.handleInputChange}/>
+            </div>
         )
     }
 }
